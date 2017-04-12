17cf44ea892f070ac769334d928a27a1f7c8bd8b
Ivan Pantic <ivanpantic82@gmail.com>
2017-04-12 22:13:22 +0200

    Cleanup of base template

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -35,15 +35,15 @@ update msg model =
 -- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
+  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][
     div [ class "row" ][
       div [ class "col-xs-12" ][
         div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
+          img [ src "static/img/elm.jpg", style styles.img ] []
+          , hello model
           , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
+          , button [ class "btn btn-primary btn-lg", onClick Increment ] [
+            span[ class "glyphicon glyphicon-star" ][]
             , span[][ text "FTW!" ]
           ]
         ]
